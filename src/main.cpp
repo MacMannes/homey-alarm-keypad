9dbcf19290fce48390f90f91b904fc9f3704886e
André Mathlener <andre.mathlener@ns.nl>
2025-03-29 16:02:38 +0100

    style: add formatter rules + reformat main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,17 @@
  * Homey Alarm Keypad
  */
 
+#include <Adafruit_PCD8544.h>
 #include <Arduino.h>
 #include <Homey.h>
-#include "wifi_config.h"
 #include <Keypad.h>
 #include <Preferences.h>
-#include "tones.h"
-#include "easteregg.h"
-#include <Adafruit_PCD8544.h>
+
 #include <map>
+
+#include "easteregg.h"
+#include "tones.h"
+#include "wifi_config.h"
 
 uint8_t state = 0;
 
@@ -19,77 +21,76 @@ uint8_t display_y = 20;
 
 bool shouldHideKeyboardEntry = true;
 
-const byte ROWS = 4; // Number of rows in the keypad
-const byte COLS = 3; // Number of columns in the keypad
+const byte ROWS = 4;
+const byte COLS = 3;
 
-char keys[ROWS][COLS] = {
-        {'1','2','3'},
-        {'4','5','6'},
-        {'7','8','9'},
-        {'*','0','#'}
+char keys [ROWS][COLS] = {
+    { '1', '2', '3' },
+    { '4', '5', '6' },
+    { '7', '8', '9' },
+    { '*', '0', '#' }
 };
 
-byte rowPins[ROWS] = { 13, 12, 14, 27 }; // Connect to the row pinouts of the keypad
-byte colPins[COLS] = { 26, 25, 33 };    // Connect to the column pinouts of the keypad
+byte rowPins [ROWS] = { 13, 12, 14, 27 };
+byte colPins [COLS] = { 26, 25, 33 };
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 Preferences preferences;
 
-
-String pinCode = "0000"; // Default PIN code stored in flash memory
+String pinCode        = "0000";  // Default PIN code stored in flash memory
 String currentCommand = "";
 
 const int BUZZER_PIN = 15;
 
 // Pin configuration for Nokia 5110 display
-#define RST_PIN   16  // Reset
-#define CE_PIN    17  // Chip Enable
-#define DC_PIN    2   // Data/Command
-#define DIN_PIN   23  // Data In (MOSI)
-#define CLK_PIN   18  // Clock
-#define BL_PIN    22  // Backlight
+#define RST_PIN 16  // Reset
+#define CE_PIN  17  // Chip Enable
+#define DC_PIN  2   // Data/Command
+#define DIN_PIN 23  // Data In (MOSI)
+#define CLK_PIN 18  // Clock
+#define BL_PIN  22  // Backlight
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(CLK_PIN, DIN_PIN, DC_PIN, CE_PIN,RST_PIN );
+Adafruit_PCD8544 display = Adafruit_PCD8544(CLK_PIN, DIN_PIN, DC_PIN, CE_PIN, RST_PIN);
 
 struct KeyValue {
     int key;
     const char *value;
 };
 
-struct KeyValue stateTexts[] = {
-        {0, "THUIS"},
-        {1, "AFWEZIG"},
-        {2, "SLAPEN"},
-        {3, "ALERT"},
-        {8, "SCHEMA"},
-        {9, "DISARMED"}
+struct KeyValue stateTexts [] = {
+    { 0, "THUIS"    },
+    { 1, "AFWEZIG"  },
+    { 2, "SLAPEN"   },
+    { 3, "ALERT"    },
+    { 8, "SCHEMA"   },
+    { 9, "DISARMED" }
 };
 
 // Define constants for state values
-constexpr int HOME = 0;
-constexpr int AWAY = 1;
-constexpr int SLEEP = 2;
-constexpr int ALERT = 3;
+constexpr int HOME     = 0;
+constexpr int AWAY     = 1;
+constexpr int SLEEP    = 2;
+constexpr int ALERT    = 3;
 constexpr int SCHEDULE = 8;
 constexpr int DISARMED = 9;
 
 // Lookup table for string-to-numeric state mapping
 const std::map<String, int> eufyStateMapping = {
-    {"home", HOME},
-    {"away", AWAY},
-    {"custom_1", SLEEP},
-    {"custom_2", ALERT},
-    {"schedule", SCHEDULE},
-    {"disarmed", DISARMED}
+    { "home",     HOME     },
+    { "away",     AWAY     },
+    { "custom_1", SLEEP    },
+    { "custom_2", ALERT    },
+    { "schedule", SCHEDULE },
+    { "disarmed", DISARMED }
 };
 
 // Lookup table for state-to-trigger mapping
 const std::map<int, String> stateTriggerMapping = {
-    {HOME, "setAlarmToHome"},
-    {AWAY, "setAlarmToAway"},
-    {SLEEP, "setAlarmToSleep"},
-    {ALERT, "setAlarmToAlert"},
-    {SCHEDULE, "setAlarmToSchedule"}
+    { HOME,     "setAlarmToHome"     },
+    { AWAY,     "setAlarmToAway"     },
+    { SLEEP,    "setAlarmToSleep"    },
+    { ALERT,    "setAlarmToAlert"    },
+    { SCHEDULE, "setAlarmToSchedule" }
 };
 
 // Function prototypes
@@ -101,30 +102,29 @@ void handleEufyStateChange();
 void beep();
 void displayKeyboardEntry(char c);
 void clearKeyboardEntry();
-void parseCommand(const String& command);
-void savePinCodeToFlash(String& newPinCode);
+void parseCommand(const String &command);
+void savePinCodeToFlash(String &newPinCode);
 void playSuccessNotes();
 void playErrorNotes();
 void triggerHomey();
 
-int mapEufyState(const String& state);
+int mapEufyState(const String &state);
 
-void changeAlarmState(int newState, const char* message);
-void handlePinChange(const String& command, int separatorIndex);
+void changeAlarmState(int newState, const char *message);
+void handlePinChange(const String &command, int separatorIndex);
 void playMonkeyIslandTheme();
 void invalidCommand();
 void executeCommand(int commandValue);
 
-
 // Lookup table for command handlers
 const std::map<int, std::function<void()>> commandHandlers = {
-    {HOME,      []() { changeAlarmState(HOME, "Turning off the alarm"); }},
-    {AWAY,      []() { changeAlarmState(AWAY, "Putting the alarm in away mode"); }},
-    {SLEEP,     []() { changeAlarmState(SLEEP, "Putting the alarm into sleep mode"); }},
-    {ALERT,     []() { changeAlarmState(ALERT, "Putting the alarm into alert mode"); }},
-    {SCHEDULE,  []() { changeAlarmState(SCHEDULE, "Putting the alarm on scheduled mode"); }},
-    {99,        []() { handlePinChange("your_command_string_here", 0); }},
-    {1990,      playMonkeyIslandTheme}
+    { HOME,     []() { changeAlarmState(HOME, "Turning off the alarm"); }                   },
+    { AWAY,     []() { changeAlarmState(AWAY, "Putting the alarm in away mode"); }          },
+    { SLEEP,    []() { changeAlarmState(SLEEP, "Putting the alarm into sleep mode"); }      },
+    { ALERT,    []() { changeAlarmState(ALERT, "Putting the alarm into alert mode"); }      },
+    { SCHEDULE, []() { changeAlarmState(SCHEDULE, "Putting the alarm on scheduled mode"); } },
+    { 99,       []() { handlePinChange("your_command_string_here", 0); }                    },
+    { 1990,     playMonkeyIslandTheme                                                       }
 };
 
 void setup() {
@@ -144,11 +144,12 @@ void setup() {
     // Read the PIN code from flash memory
     pinCode = preferences.getString("pinCode", "0000");
 
-    String deviceName = "keypad-" + String(ESP.getEfuseMac()); // Generate device name based on ID
+    // Generate device name based on ID
+    String deviceName = "keypad-" + String(ESP.getEfuseMac());
 
     Serial.println("\n\n\nDevice: " + deviceName);
 
-    //Connect to network
+    // Connect to network
     WiFiClass::setHostname(deviceName.c_str());
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.println("Connecting WiFi");
@@ -157,7 +158,7 @@ void setup() {
         Serial.print(".");
     }
 
-    //Print IP address
+    // Print IP address
     Serial.print("Connected. IP address: ");
     Serial.println(WiFi.localIP());
 
@@ -167,7 +168,7 @@ void setup() {
     Homey.setClass("remote");
 
     Homey.addAction("Set Alarm State", setState);
-    Homey.addAction("handleEufyStateChange",  handleEufyStateChange);
+    Homey.addAction("handleEufyStateChange", handleEufyStateChange);
     Homey.addCondition("Get Alarm State", getState);
 }
 
@@ -177,7 +178,7 @@ void loop() {
     char key = keypad.getKey();
 
     if (key) {
-        beep(); // Produce a beep sound
+        beep();  // Produce a beep sound
         displayKeyboardEntry((shouldHideKeyboardEntry) ? '*' : key);
         Serial.println(key);
 
@@ -189,17 +190,17 @@ void loop() {
             clearKeyboardEntry();
             // Process the command when # is pressed
             parseCommand(currentCommand);
-            currentCommand = ""; // Clear the current command
+            currentCommand          = "";  // Clear the current command
             shouldHideKeyboardEntry = true;
         } else {
-            currentCommand += key; // Append key to the current command
+            currentCommand += key;  // Append key to the current command
         }
     }
 }
 
-void parseCommand(const String& command) {
+void parseCommand(const String &command) {
     int separatorIndex = command.indexOf('*');
-    String enteredPin = "";
+    String enteredPin  = "";
 
     if (separatorIndex != -1) {
         // Extract PIN code
@@ -212,8 +213,6 @@ void parseCommand(const String& command) {
     Serial.println(pinCode);
 
     if (enteredPin.equals(pinCode)) {
-        // Check if entered PIN is correct
-
         // Set the default command value to 0
         int commandValue = 0;
 
@@ -221,7 +220,7 @@ void parseCommand(const String& command) {
         if (separatorIndex < command.length() - 1) {
             // Extract numeric command after the * character
             String numericCommand = command.substring(separatorIndex + 1);
-            commandValue = numericCommand.toInt();
+            commandValue          = numericCommand.toInt();
         }
 
         executeCommand(commandValue);
@@ -231,17 +230,13 @@ void parseCommand(const String& command) {
     }
 }
 
-
 void savePinCodeToFlash(String &newPinCode) {
     // Write the new PIN code to flash memory
     preferences.putString("pinCode", newPinCode);
     preferences.end();
 }
 
-
-void beep() {
-    tone(BUZZER_PIN, NOTE_B3, 50);
-}
+void beep() { tone(BUZZER_PIN, NOTE_B3, 50); }
 
 void clearKeyboardEntry() {
     display.fillRect(0, 20, 84, 48, WHITE);
@@ -253,7 +248,7 @@ void clearKeyboardEntry() {
 void displayKeyboardEntry(char c) {
     display.setTextSize(1);
     display.setTextColor(BLACK);
-    display.setCursor(display_x,display_y);
+    display.setCursor(display_x, display_y);
     display.print(c);
     display.display();
     display_x += 6;
@@ -270,19 +265,20 @@ void displayState() {
     uint16_t w, h;
 
     // Determine the size of the array
-    size_t arraySize = sizeof(stateTexts) / sizeof(stateTexts[0]);
+    size_t arraySize = sizeof(stateTexts) / sizeof(stateTexts [0]);
 
     const char *statusText = "ERROR";
 
     // Search the array for the entry for the current state
     for (size_t i = 0; i < arraySize; ++i) {
-        if (stateTexts[i].key == state) {
-            statusText = stateTexts[i].value;
+        if (stateTexts [i].key == state) {
+            statusText = stateTexts [i].value;
             break;
         }
     }
 
-    display.getTextBounds(statusText, x, y, &x1, &y1, &w, &h); //calc width of new string
+    // calc width of new string
+    display.getTextBounds(statusText, x, y, &x1, &y1, &w, &h);
     display.setCursor(x - w / 2, y);
     display.setTextColor(WHITE);
     display.print(statusText);
@@ -298,9 +294,7 @@ void playSuccessNotes() {
     tone(BUZZER_PIN, NOTE_G5, 100);
 }
 
-void playErrorNotes() {
-    tone(BUZZER_PIN, NOTE_A2, 500);
-}
+void playErrorNotes() { tone(BUZZER_PIN, NOTE_A2, 500); }
 
 void setState() {
     state = Homey.value.toInt();
@@ -321,25 +315,25 @@ void handleEufyStateChange() {
 }
 
 void getState() {
-    Serial.println("getState(): state is "+String(state));
+    Serial.println("getState(): state is " + String(state));
     return Homey.returnResult(state);
 }
 
-String normalizeString(const String& input) {
+String normalizeString(const String &input) {
     String result = input;
-    result.trim();   // Remove leading/trailing spaces
-    result.toLowerCase(); // Convert to lowercase
+    result.trim();         // Remove leading/trailing spaces
+    result.toLowerCase();  // Convert to lowercase
     return result;
 }
 
-int mapEufyState(const String& state) {
+int mapEufyState(const String &state) {
     String normalizedState = normalizeString(state);
 
     auto it = eufyStateMapping.find(normalizedState);
     if (it == eufyStateMapping.end()) {
         Serial.print("Invalid eufy state: ");
         Serial.println(state);
-        return -1; 
+        return -1;
     }
     return it->second;
 }
@@ -354,14 +348,12 @@ void applyState() {
 void triggerHomey() {
     auto triggerMapping = stateTriggerMapping.find(state);
     if (triggerMapping == stateTriggerMapping.end()) {
-        return; 
+        return;
     }
-
 
     String trigger = triggerMapping->second;
     Serial.println("Trigger Homey: " + trigger);
     Homey.trigger(trigger, true);
-
 }
 
 void executeCommand(int commandValue) {
@@ -370,21 +362,21 @@ void executeCommand(int commandValue) {
         invalidCommand();
         return;
     }
-    it->second(); // Call the function mapped to the command
+    it->second();  // Call the function mapped to the command
 }
 
-void changeAlarmState(int newState, const char* message) {
+void changeAlarmState(int newState, const char *message) {
     Serial.println(message);
     state = newState;
     applyState();
     playSuccessNotes();
 }
 
-void handlePinChange(const String& command, int separatorIndex) {
+void handlePinChange(const String &command, int separatorIndex) {
     int newPinIndex = command.indexOf('*', separatorIndex + 1);
     if (newPinIndex != -1) {
         String newPin = command.substring(newPinIndex + 1);
-        pinCode = newPin;
+        pinCode       = newPin;
         Serial.print("PIN code changed to: ");
         Serial.println(pinCode);
         savePinCodeToFlash(pinCode);
@@ -397,11 +389,10 @@ void handlePinChange(const String& command, int separatorIndex) {
 
 void playMonkeyIslandTheme() {
     Serial.println("Playing Monkey Island Theme :-D");
-    playMonkeyIslandTune(9); // Replace with actual buzzer pin
+    playMonkeyIslandTune(9);  // Replace with actual buzzer pin
 }
 
 void invalidCommand() {
     Serial.println("Invalid command");
     playErrorNotes();
 }
-
